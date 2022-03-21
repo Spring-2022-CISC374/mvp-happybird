aff14d583ea358e7f9bcf4def459c1897aca8cc1
Riyara Master <rtmaster@udel.edu>
2022-03-21 15:36:28 -0400

    Added title screen, play field, and movable bird.

---
+++ Field.js
@@ -0,0 +1,39 @@
+class Field extends Phaser.Scene {
+    constructor() {
+        super("playGame");
+    }
+
+    create() {
+        this.add.text(20, 20, "Use the arrow keys to move the bird.");
+        this.redbird = this.physics.add.sprite(config.width/2, config.height/2, "redbird");
+        this.redbird.setCollideWorldBounds(true);
+
+        this.cursorKeys = this.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        this.moveBird();
+    }
+
+    moveBird() {
+        if(this.cursorKeys.left.isDown) {
+            this.redbird.setVelocityX(-globalSettings.playerSpeed);
+            this.redbird.angle = 270;
+        }else if (this.cursorKeys.right.isDown) {
+            this.redbird.setVelocityX(globalSettings.playerSpeed);
+            this.redbird.angle = 90;
+        }else {
+            this.redbird.setVelocityX(0);
+        }
+
+        if(this.cursorKeys.up.isDown) {
+            this.redbird.setVelocityY(-globalSettings.playerSpeed);
+            this.redbird.angle = 0;
+        }else if (this.cursorKeys.down.isDown) {
+            this.redbird.setVelocityY(globalSettings.playerSpeed);
+            this.redbird.angle = 180;
+        }else {
+            this.redbird.setVelocityY(0);
+        }
+    }
+}
---
+++ Title.js
@@ -0,0 +1,24 @@
+class Title extends Phaser.Scene {
+    constructor() {
+        super("bootGame");
+    }
+
+    preload() {
+        this.load.spritesheet("redbird", "assets/spritesheets/redbird.png", {
+            frameWidth: 20,
+            frameHeight: 18
+        });
+    }
+
+    create() {
+        this.add.text(config.width / 2 - 100, 100, "Happy Bird", { fontSize: "32px"});
+        
+        this.startButton = this.add.text(config.width / 2 - 80, 400, "Click to start");
+        this.startButton.setInteractive();
+        this.input.on('gameobjectdown', this.start, this);
+    }
+
+    start() {
+        this.scene.start("playGame");
+    }
+}
