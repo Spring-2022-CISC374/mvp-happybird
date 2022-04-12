beed06cbe80679047e667180ed51adf843feaef1
Kevin Peachman <kpeach@udel.edu>
2022-04-11 20:25:10 -0400

    Updates

---
+++ dev/precache-manifest.4d112ecb348d7ec6b8e860bd8105ebbe.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290bbc67d0dd60b47d80185e621452f0",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devassets/badfood.png"
+  },
+  {
+    "revision": "6fb1df45fb3cc140a1a766b5085a832a",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devassets/good food.png"
+  },
+  {
+    "revision": "c5e118b560a31ce79dda7c6d455e98e6",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devassets/mediumfood.png"
+  },
+  {
+    "revision": "fe6eca5cc81b5409c125e5ee3e976ddf",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devassets/sky.png"
+  },
+  {
+    "revision": "5907a8aea94efe5aaef11fed62f968ba",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devassets/spritesheets/appleseed.png"
+  },
+  {
+    "revision": "5bf7170219878c9a363ea272082400d4",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devassets/spritesheets/birdseed.png"
+  },
+  {
+    "revision": "bf337a8fbf0851baf5fb317ccb60e182",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devassets/spritesheets/blueberry.png"
+  },
+  {
+    "revision": "c61a7052ac262e40efbeeae5c42a726e",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devassets/spritesheets/catterpillar.png"
+  },
+  {
+    "revision": "f073e5103de4e19689da8a25f2db38bb",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devassets/spritesheets/leaf.png"
+  },
+  {
+    "revision": "065887965c4dcb819cdd6beccf544cf6",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devassets/spritesheets/redBerry.png"
+  },
+  {
+    "revision": "6c8a2e3d389645d9b16fd4e4d95a855c",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devassets/spritesheets/redbird.png"
+  },
+  {
+    "revision": "a12d3af6cdda32075189746168f9f2e3",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devassets/spritesheets/stick.png"
+  },
+  {
+    "revision": "a9a19186a0171d23ec42e4e57f046926",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devassets/spritesheets/stick2.png"
+  },
+  {
+    "revision": "a12d3af6cdda32075189746168f9f2e3",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devassets/stick.png"
+  },
+  {
+    "revision": "60656f04b7dfa37888b4",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devgen.main.671afbc622576957c47d.bundle.js"
+  },
+  {
+    "revision": "d3fdbe75b48088ac7c257da75e9f64ad",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devindex.html"
+  },
+  {
+    "revision": "fc970d99dbb284f666257f816d28d96a",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devmanifest.json"
+  },
+  {
+    "revision": "5c62495a52b57e3b002d8dca78ffabc9",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devsrc/phaser.min.js"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/https://github.com/Spring-2022-CISC374/mvp-happybird/devsw.js"
+  }
+]);
--- dev/sw.js
+++ dev/sw.js
@@ -1,3 +1,5 @@
+importScripts("/https://github.com/Spring-2022-CISC374/mvp-happybird/devprecache-manifest.4d112ecb348d7ec6b8e860bd8105ebbe.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
 /**
  * You should only modify this, if you know what you are doing.
  * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
@@ -6,3 +8,4 @@
  * webpack/webpack.common.js
  */
 workbox.precaching.precacheAndRoute(__precacheManifest)
+
