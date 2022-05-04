c35d47cc144e18cf0d4e732e3405dc22b19d3822
Riyara Master <rtmaster@udel.edu>
2022-05-04 00:06:32 -0400

    Various HUD updates and changes, and minimum Nest functionality.

--- src/Field.js
+++ src/Field.js
@@ -15,24 +15,22 @@ class Field extends Phaser.Scene {
         this.background.displayWidth = globalSettings.worldWidth;
         this.background.displayHeight = globalSettings.worldHeight;
         this.background.setDisplayOrigin(0,0);
-        this.add.text(20, 20, "Use the arrow keys to move the bird.", {fill: 'white'});
-        this.add.text(20, 40, "Press space to interact with objects.");
-        this.blueberryscore = this.add.text(config.width-250,65, "+10");
-        this.blueberryscore.visible = false;
-        this.birdseedscore = this.add.text(config.width-250,65, "+5");
-        this.birdseedscore.visible = false;
-        this.appleseedscore = this.add.text(config.width-250,65, "-5");
-        this.appleseedscore.visible = false;
+        this.tutorial = this.add.text(20, 20, "Instructions: \n Use the arrow keys to move the bird. \n Press space to interact with objects. \n Bring sticks to a nice nesting spot to build a nest! \n\nPress enter to close this text box. ", {fill: 'white', backgroundColor: 'black'});
         this.health = 1;
-        this.healthAmount = this.add.text(config.width-250, 5, "Health: " + this.health + " / 100");
-        this.healthAmount.fixedToCamera = true;
+        this.healthAmount = this.add.text(config.width-250, 5, "Health: " + this.health + " / 100", {fill: 'white', backgroundColor: 'black'});
         this.inventory = 0;
-        this.inventoryAmount = this.add.text(config.width-250, 50, "Sticks: " + this.inventory);
+        this.inventoryAmount = this.add.text(config.width-250, 50, "Sticks: " + this.inventory, {fill: 'white', backgroundColor: 'black'});
         
-        this.nest = this.physics.add.sprite(Math.random()*(config.width-64)+64, Math.random()*(config.width-64)+64, "nest");
+        //set up nest
+        this.nestArea = this.physics.add.sprite(Math.random()*(config.width-64)+64, Math.random()*(config.width-64)+64, "nestarea");
+        this.nest = this.physics.add.sprite(this.nestArea.x, this.nestArea.y-10, "nest");
+        this.nest.visible = false;
+        this.nestPieces = 0;
 
+        //define keys
         this.cursorKeys = this.input.keyboard.createCursorKeys();
         this.spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
 
         //set arrays of food items and sticks to spawn
         this.Blueberries = this.physics.add.group({key: 'blueberry', repeat: 9});
@@ -45,9 +43,8 @@ class Field extends Phaser.Scene {
         this.redbird.setCollideWorldBounds(true);
         this.redbird.setScale(2, 2);
         this.cameras.main.startFollow(this.redbird);
-        this.speechBubble = this.add.text(this.redbird.x+20, this.redbird.y-20, "");
-
-        
+        this.speechBubble = this.add.text(this.redbird.x-10, this.redbird.y-40, "");
+        this.speechBubble.visible = false;
 
         this.spawnBlueberries();
         this.spawnBirdSeed();
@@ -60,11 +57,25 @@ class Field extends Phaser.Scene {
         this.physics.add.overlap(this.redbird, this.BirdSeed, this.BirdSeedCollision, null, this);
         this.physics.add.overlap(this.redbird, this.AppleSeed, this.AppleSeedCollision, null, this);
         this.physics.add.overlap(this.redbird, this.Stick2, this.StickCollision, null, this);
+        this.physics.add.overlap(this.redbird, this.nest, this.NestCollision, null, this);
+
+        //make health bar and other hud elements scroll with camera
+        this.hpBar.setScrollFactor(0);
+        this.tutorial.setScrollFactor(0);
+        this.healthAmount.setScrollFactor(0);
+        this.inventoryAmount.setScrollFactor(0);
     }
     
 
     update() {
+        this.closeTutorial();
         this.moveBird();
+    }
+
+    closeTutorial() {
+        if (this.enter.isDown) {
+            this.tutorial.visible = false;
+        }
     }
 
     moveBird() {
@@ -87,6 +98,12 @@ class Field extends Phaser.Scene {
         }else {
             this.redbird.setVelocityY(0);
         }
+        this.moveSpeechBubble();
+    }
+
+    moveSpeechBubble() {
+        this.speechBubble.x = this.redbird.x-10;
+        this.speechBubble.y = this.redbird.y-50;
     }
 
     spawnBlueberries(){
@@ -126,6 +143,7 @@ class Field extends Phaser.Scene {
         hpbar.y = 20;
         hpbar.fillStyle(0xFF0000, 1);
         hpbar.fillRect(0,0,200,20);
+        hpbar.scaleX = 1 / 100;
         return hpbar;
     }
 
@@ -152,42 +170,80 @@ class Field extends Phaser.Scene {
         this.inventoryAmount.setText("Sticks: " + this.inventory);
     }
 
-    blueBerryCollision(bird, blueberry){
+    /* credit to https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
+    for the sleep solution for the following functions */
+    async blueBerryCollision(bird, blueberry){
         if(this.spaceBar.isDown){
             blueberry.destroy();
             this.health += 10;
             this.updateHealth(this.health);
-            this.blueberryscore.visible = true;
+            this.speechBubble.setText("+10 Health!");
+            this.speechBubble.setStyle({fill: 'black', fontStyle: 'bold', strokeThickness: 3, stroke: 'green'});
+            this.speechBubble.visible = true;
+            await new Promise(r => setTimeout(r, 2000));
+            this.speechBubble.visible = false;
         }
     }
 
-    BirdSeedCollision(bird, seed){
+    async BirdSeedCollision(bird, seed){
         if(this.spaceBar.isDown){
             seed.destroy();
             this.health += 5;
             this.updateHealth(this.health);
+            this.speechBubble.setText("+5 Health.");
+            this.speechBubble.setStyle({fill: 'black', fontStyle: 'bold', strokeThickness: 3, stroke: 'yellow'});
+            this.speechBubble.visible = true;
+            await new Promise(r => setTimeout(r, 2000));
+            this.speechBubble.visible = false;
         }
     }
 
-    AppleSeedCollision(bird, appleSeed){
+    async AppleSeedCollision(bird, appleSeed){
         if(this.spaceBar.isDown){
             appleSeed.destroy();
             this.health -= 5;
             this.updateHealth(this.health);
+            this.speechBubble.setText("-5 Health...");
+            this.speechBubble.setStyle({fill: 'black', fontStyle: 'bold', strokeThickness: 3, stroke: 'red'});
+            this.speechBubble.visible = true;
+            await new Promise(r => setTimeout(r, 2000));
+            this.speechBubble.visible = false;
         }
     }
 
-    StickCollision(bird, stick){
+    async StickCollision(bird, stick){
         if(this.spaceBar.isDown){
             if(this.inventory == 3){
-                this.speechBubble.setPosition(bird.x+20, bird.y-20);
-                this.speechBubble.setText("My inventory is full!");
+                this.speechBubble.setText("I can't hold more sticks!");
+                this.speechBubble.setStyle({fill: 'black', fontStyle: 'bold', strokeThickness: 3, stroke: 'white'});
+                this.speechBubble.visible = true;
+                await new Promise(r => setTimeout(r, 2000));
+                this.speechBubble.visible = false;
             }
             if(this.inventory < 3){
                 stick.destroy();
                 this.inventory += 1;
                 this.updateInventory(this.inventory);
             }
+        }
+    }
+
+    async NestCollision(bird, nest) {
+        if(this.spaceBar.isDown && this.inventory > 0){
+            this.nest.visible = true;
+            if (this.nestPieces < 4) {
+                this.inventory -= 1;
+                this.nestPieces += 1;
+            }
+            else {
+                this.speechBubble.setText("My nest is already complete!");
+                this.speechBubble.setStyle({fill: 'black', fontStyle: 'bold', strokeThickness: 3, stroke: 'white'});
+                this.speechBubble.visible = true;
+                await new Promise(r => setTimeout(r, 2000));
+                this.speechBubble.visible = false;
+            }
+            this.nest.setFrame(this.nestPieces-1);
+            this.updateInventory(this.inventory);
         }
     }
 }
