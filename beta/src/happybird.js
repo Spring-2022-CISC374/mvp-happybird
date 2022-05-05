0d62a3587c63ab4645e75c40999f0b433307fa15
Kevin Peachman <kpeach@udel.edu>
2022-05-04 23:30:54 -0400

    added beta

---
+++ beta/src/happybird.js
@@ -0,0 +1,19 @@
+var globalSettings = {
+	playerSpeed: 250,
+	worldWidth: 1920*2,
+    worldHeight: 1080*2,
+}
+
+var config = {
+	width: 1600,
+	height: 900,
+	backgroundColor: 0x39c647,
+	scene: [Title, Field],
+	physics: {
+		default: "arcade",
+		arcade:{
+			debug: false
+		}
+	}
+}
+var Game = new Phaser.Game(config);
