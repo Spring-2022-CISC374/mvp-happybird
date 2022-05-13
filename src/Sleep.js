f1c671be91b4ee00bd01e229bec6768de291bc8d
Riyara Master <rtmaster@udel.edu>
2022-05-12 20:19:30 -0400

    Fixed the sleep message being offcenter

--- src/Sleep.js
+++ src/Sleep.js
@@ -4,7 +4,7 @@ class Sleep extends Phaser.Scene {
     }
 
     create() {
-        this.sleepText = this.add.text(config.width / 2 - 210, 100, "The Bird sleeps until morning safely!",{fill: '#000', fontSize: '64px', strokeThickness: 5});
+        this.sleepText = this.add.text(config.width / 2 - 725, 100, "The Bird sleeps until morning safely!",{fill: '#000', fontSize: '64px', strokeThickness: 5});
         this.continueButton = this.add.text(config.width / 2 - 80, 400, "Click to continue",{fill: '#000',backgroundColor: '#606060', fontStyle: 'bold'});
         this.continueButton.setInteractive();
         this.input.on('gameobjectdown', this.start, this);
